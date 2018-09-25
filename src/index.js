eccefe77d3e87dd2c45607de2693ef35f3278f93
Jinyan <jinyan.shen21@gmail.com>
2018-09-25 18:08:28 -0400

    for ivy to check

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require('./marker');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoianNoZW4iLCJhIjoiY2ptaTZsOHVuMDF0ODNrbTRuNGJmbHpiNyJ9.XQkEmHA4LE6xr5bCAzIcjw';
+
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+marker.addTo(map);
